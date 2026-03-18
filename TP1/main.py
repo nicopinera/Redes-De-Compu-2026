8216e0450063767acf51e7ac98506eec73df650a
Nicolas Piñera <nicolaspinera@gmail.com>
2026-03-18 17:22:28 -0300

    Agregado: Script para calcular crc por XOR

---
+++ TP1/main.py
@@ -0,0 +1,12 @@
+cadena_binaria_rx = ""
+resultado = 0
+
+# Recorremos de 4 en 4
+for i in range(0, len(cadena_binaria_rx), 4):
+    nibble = cadena_binaria_rx[i:i+4]      # Tomamos el nibble
+    valor = int(nibble,base=2)              # Convertimos de base 2 a entero
+    resultado ^= valor                  # Aplicamos XOR acumulativo
+
+# Si querés el resultado final de nuevo en binario (rellenando con ceros a la izquierda)
+print(f"Resultado final (entero): {resultado}")
+print(f"Resultado final (binario): {bin(resultado)[2:].zfill(4)}")
