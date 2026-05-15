cd47e1e0135f67770fca196008aef400a97644a3
Nicolas Piñera <nicolaspinera@gmail.com>
2026-05-15 17:02:49 -0300

    Agregado: Comentarios y timestamp en el script de cliente

--- TP4/script/client.py
+++ TP4/script/client.py
@@ -1,17 +1,28 @@
-import socket
-import json
+import socket, json, time
+from datetime import datetime
 
+# Definicion de IP y Puerto del servidor donde nos queremos conectar
 HOST = "127.0.0.1"
 PORT = 5000
 
+# Fecha y hora actual
+ahora = datetime.now()
+
+# Mensaje a enviar
 message = {
-    "nombre": "XxNETMASTERSxX",
-    "que_digo": "Hello Worldo!"
+    "nombre_grupo": "Puerto 1337",
+    "payload": "Hola servidor",
+    "timestamp": ahora.isoformat()
 }
 
+# Se crea el socket de la familia internet, con protocolo TCP
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Conexion de cliente con servidor
 client.connect((HOST, PORT))
 
+# Se convierte el diccionario a json
 client.sendall(json.dumps(message).encode("utf-8"))
 
+# Cierre de conexion
 client.close()
