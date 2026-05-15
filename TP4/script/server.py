d249d0ae7420dbf8797225446d2c5633c7d68c86
Nicolas Piñera <nicolaspinera@gmail.com>
2026-05-15 17:03:52 -0300

    Agregado: Comentarios y print en español

--- TP4/script/server.py
+++ TP4/script/server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import json
+from datetime import datetime
 
 HOST = "0.0.0.0"
 PORT = 5000
@@ -10,31 +11,35 @@ BUFFER_SIZE = 1024
 def handle_client(client_socket, client_address):
     ip_address = client_address[0]
 
-    print(f"Hello {ip_address} welcome to the server!")
+    print(f"Hola {ip_address},bienvenido al servidor!")
 
     try:
         while True:
             data = client_socket.recv(BUFFER_SIZE)
 
+            # Si el cliente envia vacio se rompe el bucle
             if not data:
                 break
 
             try:
+                # Se convierte el mensaje en un diccionario de python
                 message = json.loads(data.decode("utf-8"))
 
                 if (
-                    isinstance(message, dict)
-                    and "group" in message
+                    isinstance(message, dict) # Se verifica que sea un diccionario
+                    and "nombre_grupo" in message
                     and "payload" in message
-                    and isinstance(message["group"], str)
+                    and isinstance(message["nombre_grupo"], str)
                     and isinstance(message["payload"], str)
                 ):
-                    print(f"{message['group']}: {message['payload']}")
+                    print("---"*10)
+                    print(f"Grupo: {message['nombre_grupo']}\nMensaje: {message['payload']}\nInstante {datetime.now()}")
+                    print("---"*10)
                 else:
-                    print(f"{ip_address} wants to send an ill formatted message.")
+                    print(f"{ip_address}: Esperando mensaje en formato correcto")
 
             except json.JSONDecodeError:
-                print(f"{ip_address} wants to send an ill formatted message.")
+                print(f"{ip_address}: Esperando mensaje en formato correcto")
 
     except ConnectionResetError:
         pass
@@ -45,30 +50,38 @@ def handle_client(client_socket, client_address):
 
 
 def main():
+    # Creo el socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+    # Asigno el socket con la direccion IP y el puerto
     server_socket.bind((HOST, PORT))
+
+    # Empiezo a escuchar conexiones entrante
     server_socket.listen()
 
-    print(f"Server listening on {HOST}:{PORT}")
+    print(f"Servidor escuchando en {HOST}:{PORT}")
 
     try:
         while True:
+            # Se optiene el socket por donde van a comunicarse con el cliente y la informacion del cliente
             client_socket, client_address = server_socket.accept()
 
+            # Se crea un hilo y se le asigna la funcion del handle_cliente con el socket y la informacion del cliente
             client_thread = threading.Thread(
                 target=handle_client,
                 args=(client_socket, client_address)
             )
 
+            # Se inicia el hilo
             client_thread.start()
 
     except KeyboardInterrupt:
-        print("\nServer stopped.")
+        print("\nSe detiene el servidor")
 
     finally:
+        # Se cierra el socket
         server_socket.close()
 
 
